02f6b27d084553575525a792ca0ad27cb0ee9452
Yerko Cortes Baeza <yerkodigo@gmail.com>
2022-05-29 12:03:55 -0400

    'Escritura y lectura' challenge finished

--- 2.-DESAFIOS/EscrituraYLectura/ayudaDesafio/src/ayudaDesafio/Archivo.java
+++ 2.-DESAFIOS/EscrituraYLectura/ayudaDesafio/src/ayudaDesafio/Archivo.java
@@ -11,14 +11,85 @@ import java.util.Iterator;
 
 public class Archivo {
 
-	public static void main(String[] args) {
-//		crearFile();
-//	    lectura();
+	public static void main(String[] args) throws IOException {
+//		METODOS CREADOS LISTOS PARA USAR:
 //		crearArchivo("carpetita", "archivito");
-		buscarTexto("archivoSrc", "Hola");
+		buscarTexto("archivoSrc", "una");
 	}
 
-	public static void crearFile() {
+	public static void crearArchivo (String directorio, String fichero) {
+		File directory = new File("src/" + directorio);
+		File file = new File("src/" + directorio + "/" + fichero + ".txt" );
+
+		ArrayList<String> lista = new ArrayList<String>();
+		lista.add("Perro");
+		lista.add("Gato");
+		lista.add("Juan");
+		lista.add("Daniel");
+		lista.add("Juan");
+		lista.add("Gato");
+		lista.add("Perro");
+		lista.add("Camila");
+		lista.add("Daniel");
+		lista.add("Camila");
+
+		if ( !directory.exists() ) {
+			if ( directory.mkdirs() ) {
+				System.out.println("Directorio " + directorio + " creado con exito!");
+				try	{
+					file.createNewFile();
+					FileWriter fileW = new FileWriter(file);
+					BufferedWriter fileBW = new BufferedWriter(fileW);
+
+					for (int i = 0; i < lista.size(); i++) {
+						fileBW.write(lista.get(i));
+						fileBW.newLine();
+					}
+					fileBW.close();
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} else {
+				System.out.println("Error al crear directorio");
+			}
+		}
+	}
+
+	public static void buscarTexto (String nombreFichero, String texto) throws IOException {
+		File inputFile = new File("src/" + nombreFichero + ".txt");
+		FileReader fr = null;
+		String wordToSeach = texto;
+		String str;
+		if ( inputFile.exists() ) {
+			try	{
+				fr = new FileReader(inputFile);
+				BufferedReader br = new BufferedReader(fr);
+				int counterWord = 0;
+
+				while ( (str = br.readLine()) != null ) {
+					if (str.contains(wordToSeach)) {
+						String[] words = str.split(" ");
+						for (int i = 0; i < words.length; i++) {
+							if (words[i].contains(wordToSeach)) {
+								counterWord++;
+							}
+						}
+					}
+				}
+				System.out.println("Cantidad de repeticiones del texto -> " + counterWord);
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				fr.close();
+			}
+		} else {
+			System.out.println("El fichero ingresado no existe.");
+		}
+	}
+
+/*	public static void crearFile() {
 
 		File directorio = new File("src/carpeta");  
 		File archivo = new File("src/carpeta/texto.txt");  
@@ -68,69 +139,7 @@ public class Archivo {
 			System.out.println("Excepcion leyendo fichero : " + e);
 		}
 
-	}
+	}*/
 
-	public static void crearArchivo (String directorio, String fichero) {
-			File directory = new File("src/" + directorio);
-			File file = new File("src/" + directorio + "/" + fichero + ".txt" );
-
-			ArrayList<String> lista = new ArrayList<String>();
-			lista.add("Perro");
-			lista.add("Gato");
-			lista.add("Juan");
-			lista.add("Daniel");
-			lista.add("Juan");
-			lista.add("Gato");
-			lista.add("Perro");
-			lista.add("Camila");
-			lista.add("Daniel");
-			lista.add("Camila");
-
-			if ( !directory.exists() ) {
-				if ( directory.mkdirs() ) {
-					System.out.println("Directorio " + directorio + " creado con exito!");
-					try	{
-						file.createNewFile();
-						FileWriter fileW = new FileWriter(file);
-						BufferedWriter fileBW = new BufferedWriter(fileW);
-
-						for (int i = 0; i < lista.size(); i++) {
-							fileBW.write(lista.get(i));
-							fileBW.newLine();
-						}
-						fileBW.close();
-
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				} else {
-					System.out.println("Ocurrio un error al crear el directorio " + directorio);
-				}
-			}
-	}
-
-	public static void buscarTexto (String nombreFichero, String texto) {
-		File file = new File("src/" + nombreFichero + ".txt");
-		if ( file.exists() ) {
-			try	{
-				FileReader fileR = new FileReader(file);
-				BufferedReader fileBR = new BufferedReader(fileR);
-
-//				String data = fileBR.readLine();
-//
-//				while ( data != null ) {
-//					System.out.println(data);
-//					data = fileBR.readLine();
-//				}
-				// FALTA BUSCAR UNA PALABRA EN ESPECIFICO DEL CONTENIDO DEL ARCHIVO.
-				fileBR.close();
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} else {
-			System.out.println("El fichero ingresado no existe.");
-		}
-	}
 
 }
