ca397efa297189345d0e6bc8e3242af5458f1077
Yerko Cortes Baeza <yerkodigo@gmail.com>
2022-06-21 18:24:23 -0400

    Modularization of folders in a new repository

--- 2.-DESAFIOS/3.-FUNDAMENTOS-PROGRAMACION-JAVA/6.-ORIENTACION-A-OBJETOS-II/ReciclaJeans/src/cl/reciclaJeans/utility/Utilidad.java
+++
@@ -1,5 +0,0 @@
-package cl.reciclaJeans.utility;
-//Crear una clase cl.reciclaJeans.utility.Utilidad para usar métodos de limpieza de pantalla y tiempo de espera.
-
-public class Utilidad {
-}
--- 2.-DESAFIOS/3.-FUNDAMENTOS-PROGRAMACION-JAVA/8.-PRUEBAS-UNITARIAS-Y-TDD/PruebaSistemaDeClientes/src/main/java/cl/yerko/model/CategoriaEnum.java
+++
@@ -1,5 +0,0 @@
-package cl.yerko.model;
-
-public enum CategoriaEnum {
-    ACTIVO, INACTIVO;
-}
