1d9f28ae275f4266501ef65a73ecb847b018a8d6
Yerko Cortes Baeza <yerkodigo@gmail.com>
2022-06-25 13:24:56 -0400

    Added insertarInscripciones method into InscripcionDAO class

--- 4.-DESARROLLO-DE-APLICACIONES-WEB-DINAMICAS-JAVA/2.-PATRONES-DE-DISENO/EXERCISES/ListarInscripciones/src/main/java/com/edutecno/dao/FormaDePagoDAO.java
+++ 4.-DESARROLLO-DE-APLICACIONES-WEB-DINAMICAS-JAVA/2.-PATRONES-DE-DISENO/EXERCISES/ListarInscripciones/src/main/java/com/edutecno/dao/FormaDePagoDAO.java
@@ -1,5 +1,54 @@
 package com.edutecno.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.edutecno.model.FormaPagoDTO;
+
+// Tiene la responsabilidad de acceder a la base de datos y consultar por las formas de pago
 public class FormaDePagoDAO {
+	
+	// Metodo para consultar todas las formas de pago, los registros existentes en la base de datos
+	public List<FormaPagoDTO> obtieneFormasDePago() throws SQLException, ClassNotFoundException {
+		// lista para agregar las formas de pago existentes
+		List<FormaPagoDTO> listaFormaPago = new ArrayList<FormaPagoDTO>();
+		
+		
+		Connection conn = null;
+
+		//clase del driver de conexion
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		//"jdbc:mysql://localhost/DATABASENAME?user=USERNAME&password=MYPASSWORD"
+		conn = DriverManager.getConnection("jdbc:mysql://localhost/edutecno?user=root&password=mypass");
+		
+		try {
+			// Preparar la query con el PreparedStatement y la conexion
+			PreparedStatement pstm = conn.prepareStatement("SELECT * FROM forma_pago");
+			ResultSet rs = pstm.executeQuery(); //executeQuery SELECT y retorna un ResultSet
+			
+			while(rs.next()) { //Mientras el rs siga teniendo valores, que son los registros en cada fila
+				// Se instancia el objeto a construir mediante el ResultSet
+				FormaPagoDTO formaDePago = new FormaPagoDTO();
+				formaDePago.setIdFormaPago(rs.getString("id_forma_pago")); // Entre comillas va el nombre de la columna en la base de datos
+				formaDePago.setDescripcion(rs.getString("descripcion"));
+				formaDePago.setRecargo(rs.getString("recargo"));
+
+				listaFormaPago.add(formaDePago);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			conn.close();
+		}
+
+		return listaFormaPago;
+	}
 
 }
--- 4.-DESARROLLO-DE-APLICACIONES-WEB-DINAMICAS-JAVA/2.-PATRONES-DE-DISENO/EXERCISES/ListarInscripciones/src/main/java/com/edutecno/dao/InscripcionDAO.java
+++ 4.-DESARROLLO-DE-APLICACIONES-WEB-DINAMICAS-JAVA/2.-PATRONES-DE-DISENO/EXERCISES/ListarInscripciones/src/main/java/com/edutecno/dao/InscripcionDAO.java
@@ -1,5 +1,63 @@
 package com.edutecno.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.edutecno.model.InscripcionDTO;
+
+// Tendra la responsabilidad de obtener todas las inscripciones, los registros existentes
+// Responsabilidad de insertar las inscripciones
 public class InscripcionDAO {
+
+	// Metodo para inbsertar una inscripcion en la base de datos
+	public InscripcionDTO insertarInscripciones(InscripcionDTO inscripcionDTO) throws ClassNotFoundException, SQLException {
+		
+		int maxId = 0;
+
+		Connection conn = null;
+
+		// clase del driver de conexion
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		// "jdbc:mysql://localhost/DATABASENAME?user=USERNAME&password=MYPASSWORD"
+		conn = DriverManager.getConnection("jdbc:mysql://localhost/edutecno?user=root&password=mypass");
+
+		String consultaProximoId = "SELECT MAX(id_inscripcion) FROM inscripcion";
+
+		try {
+//			"INSERT INTO inscripcion (nombre, telefono, id_curso, id_forma_pago) VALUES (?, ?, ?, ?)"
+			PreparedStatement pstm = conn.prepareStatement(
+					"INSERT INTO inscripcion (id_inscripcion, nombre, telefono, id_curso, id_forma_pago) VALUES (NULL, ?, ?, ?, ?)");
+			// Seteo de valores para los signos de interrogacion en el mismo orden de la
+			// query
+			pstm.setString(1, inscripcionDTO.getNombre());
+			pstm.setString(2, inscripcionDTO.getTelefono());
+			pstm.setInt(3, inscripcionDTO.getIdCurso());
+			pstm.setInt(4, inscripcionDTO.getIdFormaPago());
+			
+			
+			if (pstm.executeUpdate() == 1) { // Si executeUpdate retorna 1, la query se ejecuto, si retorna 0 no se ejecuta
+				PreparedStatement pstm2 = conn.prepareStatement(consultaProximoId);
+				ResultSet rs = pstm2.executeQuery();
+				if (rs.next()) {
+					maxId = rs.getInt(1);
+					inscripcionDTO.setIdCurso(maxId);
+				}
+			} else {
+				throw new RuntimeException("Ha ocurrido un error insertando la inscripcion");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			conn.close();
+		}
+		
+		return inscripcionDTO;
+
+	}
 
 }
