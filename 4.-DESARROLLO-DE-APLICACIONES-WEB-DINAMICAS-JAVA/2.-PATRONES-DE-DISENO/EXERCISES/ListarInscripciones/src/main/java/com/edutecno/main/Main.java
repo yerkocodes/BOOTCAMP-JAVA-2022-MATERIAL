6a54f7c7604348078f2757bd370bab91656d5d77
Yerko Cortes Baeza <yerkodigo@gmail.com>
2022-06-25 17:11:07 -0400

    Main package and Main class added to test Data Access Object

---
+++ 4.-DESARROLLO-DE-APLICACIONES-WEB-DINAMICAS-JAVA/2.-PATRONES-DE-DISENO/EXERCISES/ListarInscripciones/src/main/java/com/edutecno/main/Main.java
@@ -0,0 +1,14 @@
+package com.edutecno.main;
+
+import java.sql.SQLException;
+
+import com.edutecno.dao.InscripcionDAO;
+import com.edutecno.model.InscripcionDTO;
+
+public class Main {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//		InscripcionDAO inscripcionDAO = new InscripcionDAO();
+//		InscripcionDTO inscripcionDTO = inscripcionDAO.insertarInscripciones(new InscripcionDTO(0, "Foo", "123456789", 1, 1));
+//		System.out.println(inscripcionDTO.getIdInscripcion());
+	}
+}
